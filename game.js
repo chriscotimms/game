5bb0d5b471f276c8cd2322d36af33ad83688e703
Chris Timms <chriscotimms@gmail.com>
2023-02-21 16:06:09 +0000

    intiial update

---
+++ game.js
@@ -0,0 +1,5 @@
+const section = document.querySelectorSelector("section");
+const playerLivesCount = document.querySelectorSelector("span");
+const playerLives = 6;
+
+playerLivesCount.textContent = playerLives;
